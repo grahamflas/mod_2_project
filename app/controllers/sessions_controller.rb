6616fc0a89cfebf2397c1e9cfda2e92da4acb653
kh45 <khaledhassan45@gmail.com>
2019-10-24 09:48:46 -0400

    merged master into add event model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,6 +9,7 @@ class SessionsController < ApplicationController
     @session = Session.find(params[:id])
   end
 
+    ## WHY DOES THIS NOT REQUIRE STRONG PARAMETERS I DON'T UNDERSTAND
   def pick_users
     @session = Session.find(params[:session_id])
     if params.keys.include?("search")
