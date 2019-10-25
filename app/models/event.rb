8d91060f00dad7e5d881d3638e29c6ebacee99dd
Graham Flaspoehler <grahamflas@gmail.com>
2019-10-25 10:04:14 -0400

    Moved search by name conditional up so that it is in same order as on page

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,15 +4,14 @@ class Event < ApplicationRecord
 
     def self.search(search)
         if search
-            if search.keys.first == "min_miles_run"
+            if search.keys.first == "name"
+                events = Event.where(name: search[:name])
+            elsif search.keys.first == "min_miles_run"
                 events = Event.where("min_miles_run >= ? ", search.values.first)
             elsif search.keys.first == "max_distance"
                 events = Event.where("distance <= ?", search.values.first)
             elsif search.keys.first == "min_distance"
                 events = Event.where("distance >= ?", search.values.first)
-            elsif search.keys.first == "name"
-                events = Event.where(name: search[:name])
-
             end
         else
           return Event.all
